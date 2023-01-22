ff4fd19b9cb60291a17b65764c59a42c33667d96
limhangyeol <hang19663@gmail.com>
2023-01-23 00:07:01 +0900

    Synchronized 를 이용한 재고감소 동시성 처리

--- src/main/kotlin/com/tommy/stock/service/StockService.kt
+++ src/main/kotlin/com/tommy/stock/service/StockService.kt
@@ -10,7 +10,12 @@ class StockService(
     private val stockRepository: StockRepository,
 ) {
 
+    /**
+     * Synchronized 는 각 프로세스 안에서만 보장되기 때문에 여러 스레드에서 동시에 데이터에 접근하게 되면 Race Condition 이 발생하게 된다.
+     * 운영 서비스는 보통 2대 이상의 서버를 사용하기 때문에 Synchronized 를 사용하지 않는다.
+     */
     @Transactional
+    @Synchronized
     fun decrease(id: Long, quantity: Long) {
         val stock = stockRepository.findByIdOrNull(id) ?: throw RuntimeException("not found stock. stock id is $id")
         stock.decrease(quantity)
